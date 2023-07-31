c95d83bd728c8bce3af8e19e5a5716c51459759b
anapacker <anaamaria.packer@gmail.com>
2023-07-31 12:24:54 -0300

    bug de birthday corrigido

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -4,10 +4,10 @@ import { db } from '../database.js'
 export async function getRentals(req, res) {
     try {
         const rentals = await db.query(`
-        SELECT rentals.*, customer.id AS "customerId", customer.name AS "customerName", game.id AS "gameId", game.name AS "gameName";
+        SELECT rentals.*, customers.id AS "customerId", customers.name AS "customerName", games.id AS "gameId", games.name AS "gameName"
         FROM rentals
-        JOIN customers ON rentals.customerId = customers.id
-        JOIN games ON rentals.gameId = games.id;
+        JOIN customers ON rentals."customerId" = customers.id
+        JOIN games ON rentals."gameId" = games.id;
         `)
         res.send(rentals.rows)
     } catch (err) {
@@ -19,13 +19,27 @@ export async function createRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
     try {
-        const priceGame = await db.query(`SELECT games."pricePerDay" FROM games WHERE id = $1;`, [gameId])
+        const customer = await db.query(`SELECT * FROM customers WHERE id = $1;`, [customerId])
+        if (customer.rows.length === 0) {
+            return res.sendStatus(400)
+        }
+        const game = await db.query(`SELECT * FROM games WHERE id =$1;`, [gameId])
+        if (game.rows.length === 0) {
+            return res.sendStatus(400)
+        }
+        if (daysRented <= 0) {
+            return res.sendStatus(400)
+        }
+
+        const rentedGames = await db.query(`SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL;`, [gameId])
+
+        const pricePerDay = game.rows[0].pricePerDay
         const rentDate = dayjs().format('YYYY-MM-DD')
-        const originalPrice = priceGame * daysRented
+        const originalPrice = pricePerDay * daysRented
 
         await db.query(`
             INSERT INTO games ("customerId", "gameId","rentDate","daysRented","returnDate", "originalPrice", "delayFee") 
-            VALUES ($1, $2, $4,null, $5, null);
+            VALUES ($1, $2, $3, $4, NULL, $5, NULL);
         `, [customerId, gameId, rentDate, daysRented, originalPrice])
 
         res.sendStatus(201)
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { db } from '../database.js'
 
 export async function getCustomers(req, res) {
